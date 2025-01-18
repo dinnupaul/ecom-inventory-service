7492aa0e0a669e0a159e710366d386eb8f816bff
USER <divyamariyampaul@gmail.com>
2025-01-19 00:40:29 +0530

    inventory changes

--- src/main/java/com/ecom/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/com/ecom/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,21 @@
 package com.ecom.inventoryservice;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.client.ReactorResourceFactory;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @PreDestroy
+    public void cleanUp() {
+        ReactorResourceFactory resourceFactory = new ReactorResourceFactory();
+        resourceFactory.destroy();
     }
 
 }
